c5a749b88f1f27814a28a3d0d34a1172a3fac6b0
Ali Sadeghian <aliosdn@gmail.com>
2024-08-15 00:11:38 -0400

    Initial commit

---
+++ app/src/main/java/com/alisdn/broadcastsandroid/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.alisdn.broadcastsandroid
+
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alisdn.broadcastsandroid.ui.theme.BroadcastsAndroidTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val airPlaneModeReceiver = AirPlaneModeReceiver()
+    private val testReceiver = TestReceiver()
+
+     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        registerReceiver(
+            airPlaneModeReceiver,
+            IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        )
+        registerReceiver(
+            testReceiver,
+            IntentFilter("TEST_ACTION"), RECEIVER_EXPORTED
+        )
+        enableEdgeToEdge()
+        setContent {
+            BroadcastsAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(airPlaneModeReceiver)
+        unregisterReceiver(testReceiver)
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BroadcastsAndroidTheme {
+        Greeting("Android")
+    }
+}
